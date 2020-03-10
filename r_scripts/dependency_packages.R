727cdccd3ec9d525bba3a24e1eccfcaee21c96e9
Reto Zihlmann <retozihlmann@outlook.com>
2020-03-10 23:53:53 +0100

    regr chapter

---
+++ r_scripts/dependency_packages.R
@@ -0,0 +1,10 @@
+
+devtools::install_git("https://github.com/retodomax/FunRZ")
+devtools::install_git("https://github.com/retodomax/DataRZ")
+install.packages("lme4")
+install.packages("lmerTest")
+install.packages("MASS")
+install.packages("car")
+install.packages("faraway")
+
+
